b8672718cb4838933ff2b228ec8e628576dd50cf
Jeremy <kwiekjeremy@gmail.com>
2021-09-07 16:54:08 -0400

    Get-QueryResults setup, with test query

--- Get-QueryResults.ps1
+++ Get-QueryResults.ps1
@@ -1,42 +1,80 @@
-#############
-# Get token #
-#############
-$tenantId = '00000000-0000-0000-0000-000000000000' # Paste your own tenant ID here
-$appId = '11111111-1111-1111-1111-111111111111' # Paste your own app ID here
-$appSecret = '22222222-2222-2222-2222-222222222222' # Paste your own app secret here
+param (
+    [Parameter(Mandatory=$true)]
+    $QueryPath,
+    $OutputType,
+    $OutputPath
+)
 
-$resourceAppIdUri = 'https://api.securitycenter.microsoft.com'
-$oAuthUri = "https://login.microsoftonline.com/$TenantId/oauth2/token"
-$body = [Ordered] @{
-    resource = "$resourceAppIdUri"
-    client_id = "$appId"
-    client_secret = "$appSecret"
-    grant_type = 'client_credentials'
+Function Get-Token {
+    if (-Not (test-path ".\creds.secret")) {
+        $tenantId = Read-Host "Enter Tenant ID"
+        $clientId =  Read-Host "Enter Application ID"
+        $appSecret = Read-Host "Enter Client Secret" -AsSecureString | ConvertFrom-SecureString
+        Set-Content -Path .\creds.secret -Value "$tenantId,$clientId,$appSecret"
+    }
+    $tenantId, $clientId, $appSecret = (Get-Content '.\creds.secret').Split(',')
+    $appSecret = $appSecret | ConvertTo-SecureString
+
+    # This code gets the application context token
+    $resourceAppIdUri = 'https://api.security.microsoft.com'
+    $oAuthUri = "https://login.windows.net/$tenantId/oauth2/token"
+    $authBody = [Ordered] @{
+        resource = $resourceAppIdUri
+        client_id = $clientId
+        client_secret = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($appSecret))
+        grant_type = 'client_credentials'
+    }
+    try {
+        $authResponse = Invoke-RestMethod -Method Post -Uri $oAuthUri -Body $authBody -ErrorAction Stop
+        return $authResponse.access_token
+    }
+    catch {
+        Write-Host "Provided credentials did not work... Please try again." -ForegroundColor Yellow
+        Write-Host $PSItem.ErrorDetails.Message -ForegroundColor Red
+        Remove-Item -Path .\creds.secret
+        Exit
+    }
 }
-$response = Invoke-RestMethod -Method Post -Uri $oAuthUri -Body $body -ErrorAction Stop
-$aadToken = $response.access_token
 
-#############
-# Run Query #
-#############
-$query = [IO.File]::ReadAllText("C:\myQuery.txt"); # Replace with the path to your file
-
-$url = "https://api.securitycenter.microsoft.com/api/advancedqueries/run"
-$headers = @{
-    'Content-Type' = 'application/json'
-    Accept = 'application/json'
-    Authorization = "Bearer $aadToken"
+Function Get-Query {
+    $queryRaw = Get-Content $QueryPath
+    foreach ($line in $queryRaw) {
+        $Query = $Query + $line
+    }
+    return $Query
 }
-$body = ConvertTo-Json -InputObject @{ 'Query' = $query }
-$webResponse = Invoke-WebRequest -Method Post -Uri $url -Headers $headers -Body $body -ErrorAction Stop
-$response =  $webResponse | ConvertFrom-Json
-$results = $response.Results
-$schema = $response.Schema
 
-################
-# Write Output #
-################
-# Output in CSV
-$results | ConvertTo-Csv -NoTypeInformation | Set-Content file1.csv
-# Output in JSON
-$results | ConvertTo-Json | Set-Content file1.json
+Function Invoke-Query {
+    param (
+        [string]$token,
+        [string]$query
+    )
+    $queryURI = 'https://api.security.microsoft.com/api/advancedhunting/run'
+    $queryHeaders =  [Ordered] @{
+        'Content-Type' = 'application/json'
+        Accept = 'application/json'
+        Authorization = "Bearer $token"
+    }
+
+    $body = ConvertTo-Json -InputObject @{ 'Query' = $query }
+
+    try {
+        $webResponse = Invoke-WebRequest -Method Post -Uri $queryURI -Headers $queryHeaders -Body $body -ErrorAction Stop
+        return $webResponse | ConvertFrom-Json
+    }
+    catch {
+        Write-Host "Provided query did not work... Please try again." -ForegroundColor Yellow
+        Write-Host $PSItem.ErrorDetails.Message -ForegroundColor Red
+        Exit
+    }
+}
+
+$token = Get-Token
+
+$parsedQuery = Get-Query
+$response = Invoke-Query $token $parsedQuery
+
+$response.Results
+# $response.Schema
+
+# TO DO - OutputType and OutputPath
